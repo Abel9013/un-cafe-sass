2be000dd3e8a544b66eba8f4ac7b074b9e21bda9
Abel <90abel.avr@gmail.com>
2023-03-21 18:10:06 -0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,52 @@
 const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const gulp = require('gulp');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif')
+
 function css(done){
   //Compilar sass
   //PASOS: 1) identificar archivos 2) Compilarlos 3) Guardar el .css
   src("src/scss/app.scss")
-      .pipe( sass({outputStyle: 'compressed'}) )
-      .pipe(postcss([autoprefixer()]))
-      .pipe( dest('build/css') )
-    done()
+  .pipe( sass({outputStyle: 'expanded'}) )
+  .pipe(postcss([autoprefixer()]))
+  .pipe( dest('build/css') )
+  done()
+}
+function versionWebp(){
+  // en vez de pasar por paranetro done y llamarlo al finalizar, coloco el return
+  return  src("src/img/**/*.{png,jpg}")
+            .pipe( webp() )
+            .pipe( dest("build/img") )
+}
+function versionAvif(){
+  const opciones = {
+    quality: 50
+  }
+  return  src("src/img/**/*.{png,jpg}")
+            .pipe( avif(opciones) )
+            .pipe( dest("build/img") )
+  
+}
+function imagenes(done){
+  src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe( dest('build/img') )
+  done()
 }
 function dev(){
   watch("src/scss/**/*.scss",  css)
-}
-function tareaDefault(done){
-  console.log('Soy la tarea por default')
-  done()
+  watch("src/img/**/*", imagenes)
 }
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+// exports.default = series(imagenes, versionWebp, versionAvif, css, dev );
+exports.default = series( css, dev );
